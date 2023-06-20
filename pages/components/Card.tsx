f4246a11d13d49634b2a3d3c7373efa115cdae7f
chandrashekar <kusumachandu43@gmail.com>
2023-06-20 18:50:04 +0530

    added ffmpeg library to generate a video

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import domtoimage from "dom-to-image";
-import { content } from "html2canvas/dist/types/css/property-descriptors/content";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+// import { createFFmpegCore } from "/ffmpeg-core.js";
 
 interface CardProps {
   iterations: number;
@@ -8,10 +9,8 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ iterations }) => {
   const [exporting, setExporting] = useState(false);
-  const [videoExporting, setVideoExporting] = useState(false);
-  const [videoDownloaded, setVideoDownloaded] = useState(false);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const [exportingVideo, setExportingVideo] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
   async function captureFullContent(element: any) {
     const options = {
@@ -25,23 +24,6 @@ const Card: React.FC<CardProps> = ({ iterations }) => {
       },
     };
 
-    const dataUrl = await domtoimage.toPng(element, options);
-    return dataUrl;
-  }
-
-  async function captureVideoContent(element: any) {
-    const scaleFactor = 4; // Adjust the scale factor as needed for higher resolution
-    const options = {
-      width: 1080 * scaleFactor,
-      height: 1920 * scaleFactor,
-      style: {
-        transform: `scale(${scaleFactor})`,
-        transformOrigin: "top left",
-        width: `${element.offsetWidth * scaleFactor}px`,
-        height: `${element.scrollHeight * scaleFactor}px`,
-      },
-    };
-  
     const dataUrl = await domtoimage.toPng(element, options);
     return dataUrl;
   }
@@ -69,219 +51,125 @@ const Card: React.FC<CardProps> = ({ iterations }) => {
     }
   }
 
-  // video generation function
-  // async function handleVideoExport() {
-  //   setVideoExporting(true);
+  async function handleExportVideo() {
+    setExportingVideo(true);
 
-  //   if (typeof window === "undefined") return;
-
-  //   const playgroundRef = document.getElementById("playground");
-  //   if (!playgroundRef) return;
-
-  //   try {
-  //     const canvas = canvasRef.current;
-  //     const video = videoRef.current;
-
-  //     const stream = canvas!.captureStream();
-  //     if (!stream) throw new Error("Failed to capture stream from canvas");
-
-  //     const fps = 30; // Adjust the frames per second as needed
-  //     const videoBitsPerSecond = 5000000; 
-  //   const mediaRecorder = new MediaRecorder(stream, { mimeType: "video/webm; codecs=vp9", videoBitsPerSecond})
-
-  //     // const mediaRecorder = new MediaRecorder(stream);
-  //     console.log(mediaRecorder);
-  //     const chunks: BlobPart[] = [];
-
-  //     mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
-  //     mediaRecorder.onstop = () => {
-  //       const blob = new Blob(chunks, { type: "video/webm" });
-  //       const videoURL = URL.createObjectURL(blob);
-
-  //       const downloadLink = document.createElement("a");
-  //       downloadLink.href = videoURL;
-  //       downloadLink.download = "playground_video.webm";
-
-  //       downloadLink.click();
-
-  //       setVideoDownloaded(true);
-  //       setVideoExporting(false);
-  //     };
-
-  //     mediaRecorder.start();
-
-  //     const duration = 500; // Duration to record each section (1 second)
-  //   const sectionCount = 6; // Number of sections to divide the conversation
-  //   const totalMessages = iterations; // Total number of messages
-  //   const messagesPerSection = Math.ceil(totalMessages / sectionCount);
-  //   // Set initial scrollTop position to the top of the content
-  //   playgroundRef.scrollTop = 0;
-
-  //   for (let sectionIndex = 0; sectionIndex < sectionCount; sectionIndex++) {
-  //     const startIndex = sectionIndex * messagesPerSection;
-  //     const endIndex = Math.min(
-  //       (sectionIndex + 1) * messagesPerSection,
-  //       totalMessages
-  //     );
-
-    
-  //     for (let i = startIndex; i < endIndex; i++) {
-
-  //       playgroundRef.scrollTop += 10; 
-  //       const imageURL = await captureVideoContent(playgroundRef);
-
-  //       const image = new Image();
-  //       image.src = imageURL;
-
-  //       image.onload = () => {
-  //         const context = canvas!.getContext("2d");
-
-  //         context?.clearRect(0, 0, canvas!.width, canvas!.height);
-  //         context?.drawImage(image, 0, 0, canvas!.width, canvas!.height);
-
-  //         console.log(mediaRecorder.requestData);
-
-  //         setTimeout(() => {
-  //           if (mediaRecorder.state === "recording") {
-  //             mediaRecorder.requestData();
-  //           }
-
-  //           // Check if this is the last message
-  //           if (i === endIndex - 1 && mediaRecorder.state === "recording") {
-  //             mediaRecorder.stop();
-  //           }
-  //         }, duration);
-  //       };
-
-  //       await new Promise((resolve) => setTimeout(resolve, duration));
-  //     }
-  //   }
-  //   // if(mediaRecorder.state === 'recording') {
-  //   //   mediaRecorder.stop();
-  //   // }
-
-  //   } catch (error) {
-  //     setVideoExporting(false);
-  //     console.error("Error exporting video:", error);
-  //   }
-  // }
-
-  async function handleVideoExport() {
-    setVideoExporting(true);
-  
-    if (typeof window === "undefined") return;
-  
-    const playgroundRef = document.getElementById("playground");
+    const playgroundRef = document.getElementById(
+      "playground"
+    ) as HTMLDivElement;
     if (!playgroundRef) return;
-  
-    try {
-      const canvas = canvasRef.current;
-      const video = videoRef.current;
-  
-      const stream = canvas!.captureStream();
-      if (!stream) throw new Error("Failed to capture stream from canvas");
-  
-      const fps = 30; // Adjust the frames per second as needed
-      const videoBitsPerSecond = 5000000;
-      const mediaRecorder = new MediaRecorder(stream, {
-        mimeType: "video/webm; codecs=vp9",
-        videoBitsPerSecond,
-      });
-  
-      const chunks: BlobPart[] = [];
-  
-      mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
-      mediaRecorder.onstop = () => {
-        const blob = new Blob(chunks, { type: "video/webm" });
-        const videoURL = URL.createObjectURL(blob);
-  
-        const downloadLink = document.createElement("a");
-        downloadLink.href = videoURL;
-        downloadLink.download = "playground_video.webm";
-  
-        downloadLink.click();
-  
-        setVideoDownloaded(true);
-        setVideoExporting(false);
-      };
-  
-      mediaRecorder.start();
-  
-      const duration = 1000; // Duration to record each section (1 second)
-      const sectionCount = 6; // Number of sections to divide the conversation
-      const totalMessages = iterations; // Total number of messages
-      const messagesPerSection = Math.ceil(totalMessages / sectionCount);
-  
-      // Calculate the total height of the content
-      const contentHeight = playgroundRef.scrollHeight;
 
-      console.log(contentHeight);
-  
-      // Set initial scrollTop position to the top of the content
-      playgroundRef.scrollTop = 0;
-  
-      for (let sectionIndex = 0; sectionIndex < sectionCount; sectionIndex++) {
-        const startIndex = sectionIndex * messagesPerSection;
-        const endIndex = Math.min(
-          (sectionIndex + 1) * messagesPerSection,
-          totalMessages
-        );
-  
-        // Scroll to the next section gradually while capturing frames
-        for (let i = startIndex; i < endIndex; i++) {
-          // Calculate the scroll position based on the current message index
-          const scrollPosition = contentHeight * (i / totalMessages);
-          console.log(scrollPosition);
+    const sectionHeight = playgroundRef.offsetHeight;
+    const totalHeight = playgroundRef.scrollHeight;
 
-        // Scroll to the calculated position
-        playgroundRef.scrollTo({ top: scrollPosition, behavior: "smooth" });
-  
-          // Capture the current frame
-          const imageURL = await captureVideoContent(playgroundRef);
-  
-          const image = new Image();
-          image.src = imageURL;
-  
-          image.onload = () => {
-            const context = canvas!.getContext("2d");
-            console.log(context)
-  
-            context?.clearRect(0, 0, canvas!.width, canvas!.height);
-            context?.drawImage(image, 0, 0, canvas!.width, canvas!.height);
-  
-            // setTimeout(() => {
-              if (mediaRecorder.state === "recording") {
-                console.log(mediaRecorder.requestData);
-                mediaRecorder.requestData();
-              }
-  
-              // Check if this is the last message
-              // if (i === endIndex - 1 && mediaRecorder.state === "recording") {
-              //   mediaRecorder.stop();
-              // }
-            // }, duration);
-          };
-  
-          await new Promise((resolve) => setTimeout(resolve, duration));
-        }
-      }
-  
-      if (mediaRecorder.state === "recording") {
-        mediaRecorder.stop();
-      }
-    } catch (error) {
-      setVideoExporting(false);
-      console.error("Error exporting video:", error);
+    const videoWidth = playgroundRef.offsetWidth;
+    const videoHeight = totalHeight;
+
+    const frames = [];
+
+    let scrollTop = 0;
+
+    while (scrollTop < totalHeight) {
+      playgroundRef.scrollTop = scrollTop;
+      await new Promise((resolve) => setTimeout(resolve, 100)); // Wait for the scroll animation
+
+      const dataUrl = await captureFullContent(playgroundRef);
+      frames.push(dataUrl);
+
+      scrollTop += sectionHeight / 4; // Scroll 1/4th of the section height for the next frame
     }
+
+    const tempFileNames = [];
+
+    for (let i = 0; i < frames.length; i++) {
+      const frameDataUrl = frames[i];
+      const formData = new FormData();
+
+      const imageBlob = await fetch(frameDataUrl).then((response) =>
+        response.blob()
+      );
+      const imageFileName = `frame_${i}.png`;
+      formData.append("file", imageBlob, imageFileName);
+
+      try {
+        const response = await fetch("http://localhost:3001/upload", {
+          method: "POST",
+          body: formData,
+        });
+        const fileName = await response.text();
+        tempFileNames.push(fileName);
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    }
+
+    const concatFileContent = tempFileNames
+      .map((fileName) => `file '${fileName}'`)
+      .join("\n");
+    const formData = new FormData();
+    formData.append("concatFileContent", concatFileContent);
+
+    try {
+      const response = await fetch("http://localhost:3001/upload-concat", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log("Concat file uploaded successfully");
+      } else {
+        console.error(
+          "Error uploading concat file:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+
+    // getting the response from the server
+    try {
+      const response = await fetch("/export-video", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ tempFileNames }),
+      });
+
+      if (response.ok) {
+        const outputFileName = await response.text();
+        const downloadLink = document.createElement("a");
+        downloadLink.href = `http://localhost:3001/uploads/${outputFileName}`;
+        downloadLink.download = outputFileName;
+        downloadLink.click();
+      } else {
+        console.error(
+          "Error exporting video:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+
+    for (let i = 0; i < tempFileNames.length; i++) {
+      await fetch(`http://localhost:3001/delete?file=${tempFileNames[i]}`, {
+        method: "DELETE",
+      });
+    }
+
+    setExportingVideo(false);
   }
-  
 
   const messages = ["Hello", "Hi", "how are you", "i am good"];
   const colors = ["lightblue", "lightgreen"];
   iterations = Math.floor(Math.random() * 10) + 40; // Random number between 40 and 45
 
   return (
-    <div id="playgrounds">
+    <>
       <div className="card" id="playground">
         {[...Array(iterations)].map((_, index) => (
           <div key={index} className="message py-2 rounded">
@@ -305,24 +193,21 @@ const Card: React.FC<CardProps> = ({ iterations }) => {
       <button
         className="block p-2 border rounded gap-5 mb-5"
         onClick={handleExport}
-        disabled={exporting}
+        disabled={exporting || exportingVideo}
       >
-        {exporting ? "Exporting.." : "Export"}
+        {exporting ? "Exporting..." : "Export"}
       </button>
       <div>
-        {!videoDownloaded && (
-          <button
-            className="block p-2 border rounded"
-            onClick={handleVideoExport}
-            disabled={videoExporting}
-          >
-            {videoExporting ? "Exporting video.." : "Export as video"}
-          </button>
-        )}
+        <button
+          className="block p-2 border rounded gap-5 mb-5"
+          onClick={handleExportVideo}
+          // disabled={exporting || exportingVideo}
+        >
+          {exportingVideo ? "Exporting as Video..." : "Export as Video"}
+        </button>
       </div>
-      <canvas ref={canvasRef} style={{ display: "none"}} width={1080*4} height={900 * 5} />
-      <video ref={videoRef} style={{ display: "none" }} />
-    </div>
+      <video ref={videoRef} controls />
+    </>
   );
 };
 
---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const { createFFmpeg } = require('@ffmpeg/ffmpeg');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+app.use(fileUpload());
+app.use(express.json());
+app.use(cors());
+
+const ffmpeg = createFFmpeg({ log: true });
+const uploadsDir = path.join(__dirname, 'public', 'uploads');
+fs.mkdirSync(uploadsDir, { recursive: true });
+
+app.use(express.static('public'));
+
+app.post('/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  const file = req.files.file;
+  const filePath = `${uploadsDir}/${file.name}`;
+
+  file.mv(filePath, (err) => {
+    if (err) {
+      console.error('Error uploading file:', err);
+      return res.status(500).send('Error uploading file.');
+    }
+
+    res.send(file.name);
+  });
+});
+
+server.post('/upload-concat', (req, res) => {
+  const concatFilePath = path.join(uploadsDir, 'concat-txt');
+  const concatFileContent = req.body.concatFileContent;
+  console.log(concatFileContent);
+
+  fs.writeFile(concatFilePath, concatFileContent, (err) => {
+    if (err) {
+      console.error('Error writing concat file:', err);
+      res.status(500).send('Error writing concat file');
+    } else {
+      console.log('Concat file created:', concatFilePath);
+      res.status(200).send('Concat file created successfully');
+    }
+  });
+});
+
+
+app.post('/export-video', async (req, res) => {
+  try {
+    const tempFileNames = req.body.tempFileNames;
+
+    // Load the concat file
+    const concatFilePath = path.join(uploadsDir, 'concat.txt');
+    const concatFileContent = tempFileNames.map((fileName) => `file '${uploadsDir}/${fileName}'`).join('\n');
+    fs.writeFileSync(concatFilePath, concatFileContent);
+
+    await ffmpeg.load();
+
+    // Run the ffmpeg command to concatenate frames into a video
+    await ffmpeg.run('-f', 'concat', '-safe', '0', '-i', concatFilePath, '-c', 'copy', `${uploadsDir}/output.mp4`);
+
+    res.send('output.mp4');
+  } catch (error) {
+    console.error('Error exporting video:', error);
+    res.status(500).send('Error exporting video.');
+  }
+});
+
+app.delete('/delete', (req, res) => {
+  const fileName = req.query.file;
+  const filePath = `${uploadsDir}/${fileName}`;
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error('Error deleting file:', err);
+      return res.status(500).send('Error deleting file.');
+    }
+
+    res.send('File deleted successfully.');
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
